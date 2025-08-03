57275c36d459d4cbb307c8d2f01af488db2aefa7
ojasalai27 <ojasalai2704@gmail.com>
2025-08-04 00:48:39 +0530

    frozen lake non slippery script

--- Mini_Projects/frozenlake_4x4_q.py
+++ Mini_Projects/frozenlake_4x4_q.py
@@ -1,14 +1,15 @@
 import gymnasium as gym 
 import numpy as np
 
-env = gym.make('FrozenLake-v1', desc = None, map_name = "4x4", is_slippery = False, render_mode = "human")
+env = gym.make('FrozenLake-v1', desc = None, map_name = "4x4", is_slippery = False, render_mode = None)
 
 alpha = 0.8 
 gamma = 0.95 
 epsilon = 0.95
 min_epsilon = 0.01
 decay_factor = 0.995
-episodes = 100
+episodes = 50000
+test_episodes = 1000
 
 states = env.observation_space.n
 actions = env.action_space.n
@@ -41,26 +42,32 @@ for episode in range(episodes):
 print("Training complete")
 np.save("frozenlake_4x4_qtable.npy", Q)
 
-def run():
-    state, _ = env.reset()
-    terminated = False
-    truncated = False
-    total_reward = 0
+def run(n):
+    success = 0
+    print(f"\nTesting over {n} episodes:")
+    for episode in range(n):
+        state, _ = env.reset()
+        terminated = False
+        truncated = False
+        total_reward = 0
+        print(f"\nEpisode{episode}:")
 
-    while not (terminated or truncated):
-        action = np.argmax(Q[state])
-        new_state, reward, terminated, truncated, _ = env.step(action)
-        total_reward += reward
-        state = new_state
+        while not (terminated or truncated):
+            action = Q[state].argmax()
+            new_state, reward, terminated, truncated, _ = env.step(action)
+            total_reward += reward
+            state = new_state
 
         if terminated: 
             if reward == 1:
                 print("Yay!! You reached the gift")
+                success += 1
             else:
                 print("Oops! Try again!")
 
         print(f"Final Reward: {total_reward}")
+        print(f"\nReached the goal in {success} out of {n} episodes")
     env.close()    
 
 if __name__ == '__main__':
-    run()
+    run(test_episodes)
